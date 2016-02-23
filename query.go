3b428d2898fbc83169188c90bcfc3d5ef581f5b0
Alexander Staubo <alex@bengler.no>
2016-02-23 11:44:41 -0500

    Add more fields. Use pointers.

--- query.go
+++ query.go
@@ -5,18 +5,6 @@ type GeoJSON struct {
 	Coordinates interface{} `json:"coordinates"`
 }
 
-type IntValue struct {
-	Value int
-}
-
-type BoolValue struct {
-	Value bool
-}
-
-type FloatValue struct {
-	Value float64
-}
-
 type Criterion struct {
 	Dimension                      string      `json:"dimension"`
 	Id                             interface{} `json:"id,omitempty"`
@@ -25,32 +13,32 @@ type Criterion struct {
 	ExactRequires                  string      `json:"exactRequires,omitempty"`
 	Operator                       string      `json:"operator,omitempty"`
 	Criteria                       []Criterion `json:"criteria,omitempty"`
-	MinStartsWithLength            *IntValue   `json:"minStartsWithLength,omitempty"`
-	IgnoreFieldLength              *BoolValue  `json:"ignoreFieldLength,omitempty"`
-	IgnoreInverseDocumentFrequency *BoolValue  `json:"ignoreInverseDocumentFrequency,omitempty"`
+	MinStartsWithLength            *int        `json:"minStartsWithLength,omitempty"`
+	IgnoreFieldLength              *bool       `json:"ignoreFieldLength,omitempty"`
+	IgnoreInverseDocumentFrequency *bool       `json:"ignoreInverseDocumentFrequency,omitempty"`
 	SearchStyle                    string      `json:"searchStyle,omitempty"`
 	Value                          interface{} `json:"value,omitempty"`
 	DidYouMean                     interface{} `json:"didYouMean,omitempty"`
-	Cull                           *BoolValue  `json:"cull,omitempty"`
-	Min                            *FloatValue `json:"min,omitempty"`
-	MinInclusive                   *BoolValue  `json:"minInclusive,omitempty"`
-	Max                            *FloatValue `json:"max,omitempty"`
-	MaxInclusive                   *BoolValue  `json:"maxInclusive,omitempty"`
-	Latitude                       *FloatValue `json:"latitude,omitempty"`
-	Longitude                      *FloatValue `json:"longitude,omitempty"`
+	Cull                           *bool       `json:"cull,omitempty"`
+	Min                            *float64    `json:"min,omitempty"`
+	MinInclusive                   *bool       `json:"minInclusive,omitempty"`
+	Max                            *float64    `json:"max,omitempty"`
+	MaxInclusive                   *bool       `json:"maxInclusive,omitempty"`
+	Latitude                       *float64    `json:"latitude,omitempty"`
+	Longitude                      *float64    `json:"longitude,omitempty"`
 	ZipCode                        string      `json:"zipcode,omitempty"`
 	DistanceUnit                   string      `json:"distanceUnit,omitempty"`
 	Fields                         []string    `json:"fields,omitempty"`
 	IsoRelevanceDistance           interface{} `json:"isoRelevanceDistance,omitempty"`
-	ExactMatch                     *BoolValue  `json:"exactMatch,omitempty"`
-	NullExactMatch                 *BoolValue  `json:"nullExactMatch,omitempty"`
-	ExactRelevance                 *FloatValue `json:"exactRelevance,omitempty"`
-	FuzzyRelevance                 *FloatValue `json:"fuzzyRelevance,omitempty"`
-	NullRelevance                  *FloatValue `json:"nullRelevance,omitempty"`
-	ExactDistance                  *FloatValue `json:"exactDistance,omitempty"`
-	CullDistance                   *FloatValue `json:"cullDistance,omitempty"`
-	NormalDistance                 *FloatValue `json:"normalDistance,omitempty"`
-	Weight                         *FloatValue `json:"weight,omitempty"`
+	ExactMatch                     *bool       `json:"exactMatch,omitempty"`
+	NullExactMatch                 *bool       `json:"nullExactMatch,omitempty"`
+	ExactRelevance                 *float64    `json:"exactRelevance,omitempty"`
+	FuzzyRelevance                 *float64    `json:"fuzzyRelevance,omitempty"`
+	NullRelevance                  *float64    `json:"nullRelevance,omitempty"`
+	ExactDistance                  *float64    `json:"exactDistance,omitempty"`
+	CullDistance                   *float64    `json:"cullDistance,omitempty"`
+	NormalDistance                 *float64    `json:"normalDistance,omitempty"`
+	Weight                         *float64    `json:"weight,omitempty"`
 	Placemarks                     *GeoJSON    `json:"placemarks,omitempty"`
 }
 
@@ -64,19 +52,24 @@ type IndexValue struct {
 	Dimension string `json:"dimension"`
 }
 
+type PropertyNameList []string
+
 type Query struct {
-	Criteria         []Criterion  `json:"criteria,omitempty"`
-	ExactMatchesOnly bool         `json:"exactMatchesOnly"`
-	PageSize         int          `json:"pageSize"`
-	Items            []string     `json:"items,omitempty"`
-	NotItems         []string     `json:"notItems,omitempty"`
-	ExactRelevance   *FloatValue  `json:"exactRelevance,omitempty"`
-	StartIndex       int          `json:"startIndex"`
-	Values           []string     `json:"values,omitempty"`
-	Explain          string       `json:"explain,omitempty"`
-	IndexValues      []IndexValue `json:"indexValues,omitempty"`
-	GroupBy          *GroupBy     `json:"groupBy,omitempty"`
+	Criteria         []Criterion       `json:"criteria,omitempty"`
+	ExactMatchesOnly bool              `json:"exactMatchesOnly"`
+	PageSize         int               `json:"pageSize"`
+	Items            []string          `json:"items,omitempty"`
+	NotItems         []string          `json:"notItems,omitempty"`
+	ExactRelevance   *float64          `json:"exactRelevance,omitempty"`
+	StartIndex       int               `json:"startIndex"`
+	Values           []string          `json:"values,omitempty"`
+	Explain          string            `json:"explain,omitempty"`
+	IndexValues      []IndexValue      `json:"indexValues,omitempty"`
+	GroupBy          *GroupBy          `json:"groupBy,omitempty"`
+	Properties       *PropertyNameList `json:"properties,omitempty"`
 }
+
+type PropertyMap map[string]interface{}
 
 type Results struct {
 	DatasetSize     int                               `json:"datasetSize"`
@@ -88,5 +81,6 @@ type Results struct {
 	ExactMatches    []bool                            `json:"exactMatches"`
 	RelevanceValues []float64                         `json:"relevanceValues"`
 	ItemIds         []string                          `json:"itemIds"`
+	Properties      []PropertyMap                     `json:"properties"`
 	IndexValues     map[string]map[string]interface{} `json:"indexValues"`
 }
---
+++ types.go
@@ -0,0 +1,21 @@
+package discoveryclient
+
+func Int(value int) *int {
+	return &value
+}
+
+func String(value string) *string {
+	return &value
+}
+
+func Bool(value bool) *bool {
+	return &value
+}
+
+func Float(value float64) *float64 {
+	return &value
+}
+
+func PropertyNames(s PropertyNameList) *PropertyNameList {
+	return &s
+}
